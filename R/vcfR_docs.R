cde983faad082cc3998b8526179ba9a6af7f9216
Brian J. Knaus <briank.lists@gmail.com>
2016-02-26 16:17:07 -0800

    created package with pkgkitten

---
+++ R/vcfR_docs.R
@@ -0,0 +1,13 @@
+#' vcfR_docs: Documentation for vcfR
+#' 
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name foo
+NULL
+#> NULL
