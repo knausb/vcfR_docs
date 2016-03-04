7f4e21a59f14bfc5ae7980f5c9eb5e85050289f0
Brian J. Knaus <briank.lists@gmail.com>
2016-03-04 14:07:26 -0800

    managed inst/

--- R/vcfR_docs.R
+++ R/vcfR_docs.R
@@ -3,9 +3,15 @@
 #'
 #' The vcfRdocs package provides documentation and examples for vcfR
 #' 
-#' @section section 1:
+# @section section 1:
 #'
 #' @docType package
 #' @name vcfRdocs
+#' 
+#' @import adegenet
+#' @import pinfsc50
+#' @import poppr
+#' @import vcfR
+#' 
 NULL
 #> NULL
